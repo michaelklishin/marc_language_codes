e916fa8e76c79968e7d570cc5cbb92d9a408edb6
Michael S. Klishin <michael@novemberain.com>
2009-05-01 22:47:42 +0400

    Generate lookup hashes and specifications

--- spec/marc_language_codes_spec.rb
+++ spec/marc_language_codes_spec.rb
@@ -1,7 +1,37 @@
-require 'spec_helper'
+require "pathname"
+require Pathname.new(File.dirname(__FILE__)).join('spec_helper')
 
-describe "MarcLanguageCodes" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe MARC::LanguageCodes do
+
+  #
+  # Examples for 15 language, they are too many to do
+  # them all
+  #
+  # List from  http://en.wikipedia.org/wiki/List_of_languages_by_number_of_speakers
+  # Codes from http://www.loc.gov/marc/languages/language_code.html
+  #
+
+  MARC::LanguageCodes.combined_languages_list.each_pair do |code, authoritive_name|
+    describe ".lookup(#{code})" do
+      it "returns #{authoritive_name}" do
+        MARC::LanguageCodes.lookup(code).should == authoritive_name
+      end
+    end
+  end
+
+  MARC::LanguageCodes.current_languages_list.each_pair do |code, authoritive_name|
+    describe ".lookup_current(#{code})" do
+      it "returns #{authoritive_name}" do
+        MARC::LanguageCodes.lookup_current(code).should == authoritive_name
+      end
+    end
+  end
+
+  MARC::LanguageCodes.discontinued_languages_list.each_pair do |code, authoritive_name|
+    describe ".lookup_discontinued(#{code})" do
+      it "returns #{authoritive_name}" do
+        MARC::LanguageCodes.lookup_discontinued(code).should == authoritive_name
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'marc_language_codes'
+require 'marc/language_codes'
 
 Spec::Runner.configure do |config|
   
